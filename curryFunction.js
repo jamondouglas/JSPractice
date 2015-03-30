091f7dadf1d3ca170d7b41e215510cdf78ee0015
Terril Douglas <tdouglas@administorsmbp3.mindjet.internal>
2015-03-30 13:28:47 -0700

    continue to FP refactor curry function

--- curryFunction.js
+++ curryFunction.js
@@ -6,16 +6,23 @@ function setDefaults(value,defaultValue){
 	return _.isUndefined(value) && defaultValue;
 }
 
+function addElementToCollection(collection, item){
+	return collection.concat(item);
+}
 
+function sliceArray(collection, start, end){
+	return _.slice(collection,start, end);
+}
 
 module.exports = function curryFunc(func, numOfArgs,argAccumalator){
 	numOfArgs = setDefaults(numOfArgs, func.length);
 	argAccumalator = setDefaults(argAccumalator, []);
 	return function(){
 		if(numOfArgs === arguments.length){
-			return func.apply(null,argAccumalator.concat(Array.prototype.slice.call(arguments)));
+			// return func.apply(null,argAccumalator.concat(Array.prototype.slice.call(arguments)));
+			return func.apply(null,addElementToCollection(argAccumalator, sliceArray(arguments)));
 		}else if(arguments.length < numOfArgs ){
-			argAccumalator = argAccumalator.concat(Array.prototype.slice.call(arguments,0,arguments.length));
+			argAccumalator = addElementToCollection(argAccumalator,sliceArray(arguments,0,arguments.length));
 			return curryFunc(func,numOfArgs-arguments.length,argAccumalator);
 		}
 	};
