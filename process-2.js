9248770a3a65e8ccce11519969487da310655fd9
Terril Douglas <tdouglas@Administrators-MacBook-Pro-3.local>
2015-03-29 18:56:07 -0700

    Initial commit

---
+++ process-2.js
@@ -0,0 +1,3 @@
+process.argv.forEach(function(val, key, collection){
+	console.log(key + ' : '+ val);
+});
---
+++ underscore-jam/underscore-jam.js
@@ -0,0 +1,36 @@
+
+
+//identity
+var _ = {};
+
+
+
+_.idenity = function identiy(obj){
+	return obj;
+};
+
+_.each = function  each(obj, iteratee, context){
+	var keys,index;
+	if(Array.isArray(obj)){
+		for(index = 0; index< obj.length; index++){
+			iteratee(obj[index], index, obj);
+		}
+	}else{
+		keys = Object.keys(obj);
+		for(index = 0; index < keys.length; index++){
+			iteratee(obj[keys[index]],keys[index], obj);
+		}
+	}
+};
+
+_.map = function map(obj, iteratee, context){
+	var results = [];
+	
+	_.each(obj,iteratee, context);
+	return results;
+};
+
+//HELPER FUNCTIONS
+//isArray
+
+var testFunc = (function(){return function(x){console.log("Value of x is: "+x);};});
