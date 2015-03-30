f3f7331a858c80ea706ef8c310d886c2cdcd20dc
Terril Douglas <tdouglas@administorsmbp3.mindjet.internal>
2015-03-30 13:09:05 -0700

    use lodash and start implementing FP style of coding

--- curryFunction.js
+++ curryFunction.js
@@ -1,14 +1,20 @@
+//dont use concat twice
+//pull out setting the defaults
+// pull slicing functionality
+var _ = require('lodash');
+function setDefaults(value,defaultValue){
+	return _.isUndefined(value) && defaultValue;
+}
+
 
 
 module.exports = function curryFunc(func, numOfArgs,argAccumalator){
-	numOfArgs = numOfArgs || func.length;
-	var argAccumalator = argAccumalator || [];
+	numOfArgs = setDefaults(numOfArgs, func.length);
+	argAccumalator = setDefaults(argAccumalator, []);
 	return function(){
 		if(numOfArgs === arguments.length){
-			debugger;
 			return func.apply(null,argAccumalator.concat(Array.prototype.slice.call(arguments)));
 		}else if(arguments.length < numOfArgs ){
-			debugger;
 			argAccumalator = argAccumalator.concat(Array.prototype.slice.call(arguments,0,arguments.length));
 			return curryFunc(func,numOfArgs-arguments.length,argAccumalator);
 		}
