563671da172d3e4bd3df6cdd2e6caa4bd501adf7
Terril Douglas <tdouglas@administorsmbp3.mindjet.internal>
2015-03-30 17:14:46 -0700

    extract helper functions

--- curryFunction.js
+++ curryFunction.js
@@ -2,6 +2,7 @@
 //pull out setting the defaults
 // pull slicing functionality
 var _ = require('lodash');
+
 function setDefaults(value,defaultValue){
 	return _.isUndefined(value) && defaultValue;
 }
@@ -14,13 +15,17 @@ function sliceArray(collection, start, end){
 	return _.slice(collection,start, end);
 }
 
+function lessThan(a,b){
+	return a < b;
+}
+
 module.exports = function curryFunc(func, numOfArgs,argAccumalator){
 	numOfArgs = setDefaults(numOfArgs, func.length);
 	argAccumalator = setDefaults(argAccumalator, []);
 	return function(){
-		if(numOfArgs === arguments.length){
+		if(_.isEqual(numOfArgs, arguments.length)){
 			return func.apply(null,addElementToCollection(argAccumalator, sliceArray(arguments)));
-		}else if(arguments.length < numOfArgs ){
+		}else if(lessThan(arguments.length, numOfArgs)){
 			argAccumalator = addElementToCollection(argAccumalator,sliceArray(arguments,0,arguments.length));
 			return curryFunc(func,numOfArgs-arguments.length,argAccumalator);
 		}
